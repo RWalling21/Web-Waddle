05f838b09fb0ae9b234e62be7a3c4e1b1147abf1
Robert Walling <rhw8246@rit.edu>
2023-11-26 15:08:49 -0500

    Properly structured tool

--- webwaddle/core.py
+++ webwaddle/core.py
@@ -43,35 +43,35 @@ class SearchTool(BaseTool):
     description = "useful for when you need to answer questions about current events, or verify critical information"
 
     def _run(self, query: str) -> str:
-        """Use the tool."""
-        def run_search(self, query: str) -> SearchResults:
-            # Use DuckDuckGo to search for the query
-            raw_results = search.run(query)
-            return SearchResults(results=[SearchResult(**result) for result in raw_results])
-        
-        def summarize_results(self, search_results: SearchResults) -> str:
-            # Gather and concatenate all search results
-            snippets = [r.snippet for r in search_results.results]
-            context = ' | Next Page | '.join(snippets)
-
-            # Summarize the search results
-            summary_input = SummaryInput(snippets=snippets, context=context, question=query)
-            summary = llm.generate(
-                summary_prompt_template,
-                context=summary_input.context,
-                question=summary_input.question,
-                max_tokens=750,
-            )
-
-            return summary
-        
-        results = run_search(query)
+        results = self.run_search(query)
         print(results)
 
-        summary = summarize_results(results)
+        summary = self.summarize_results(results)
         print(summary)
 
-        return run_search
+        return summary
+    
+    """Use the tool."""
+    def run_search(self, query: str) -> SearchResults:
+        # Use DuckDuckGo to search for the query
+        raw_results = search.run(query)
+        return SearchResults(results=[SearchResult(**result) for result in raw_results])
+    
+    def summarize_results(self, search_results: SearchResults) -> str:
+        # Gather and concatenate all search results
+        snippets = [r.snippet for r in search_results.results]
+        context = ' | Next Page | '.join(snippets)
+
+        # Summarize the search results
+        summary_input = SummaryInput(snippets=snippets, context=context, question=query)
+        summary = llm.generate(
+            summary_prompt_template,
+            context=summary_input.context,
+            question=summary_input.question,
+            max_tokens=750,
+        )
+
+        return summary
 
 # For testing purposes  
 tools = [SearchTool()]
@@ -79,6 +79,4 @@ agent = initialize_agent(
     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
 
-agent.run(
-    "Who is the current CEO of OpenAI?"
-)
+agent.run({"input": "Who is the current CEO of OpenAI?"})
