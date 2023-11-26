052b54e665ab945bbe97bf9c4280ed35622bb369
Robert Walling <rhw8246@rit.edu>
2023-11-26 15:03:58 -0500

    Implemented Pydantic data validation

--- webwaddle/core.py
+++ webwaddle/core.py
@@ -2,7 +2,7 @@ from typing import List
 from langchain.agents import AgentType, initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import BaseTool, DuckDuckGoSearchResults
-
+from pydantic import BaseModel, HttpUrl
 from dotenv import load_dotenv
 
 # Load OpenAI_API_KEY from env
@@ -15,7 +15,21 @@ llm = ChatOpenAI(
 )
 
 # Set search to DuckDuckGo
-search = DuckDuckGoSearchResults(backend="Educational")
+search = DuckDuckGoSearchResults()
+
+# Type definitions 
+class SearchResult(BaseModel):
+    snippet: str
+    title: str
+    link: HttpUrl
+
+class SearchResults(BaseModel):
+    results: List[SearchResult]
+
+class SummaryInput(BaseModel):
+    snippets: List[str]
+    context: str
+    question: str
 
 from utils import summary_prompt_template
 class SearchTool(BaseTool):
@@ -25,36 +39,37 @@ class SearchTool(BaseTool):
     This tool is designed to search for information relevant to current events or to verify critical information. 
     It queries DuckDuckGo with a given search term, retrieves the results, and then summarizes these results to provide a concise overview.
     """
-
     name = "search"
     description = "useful for when you need to answer questions about current events, or verify critical information"
 
     def _run(self, query: str) -> str:
         """Use the tool."""
-        def run_search(query: str):
+        def run_search(self, query: str) -> SearchResults:
             # Use DuckDuckGo to search for the query
-            return search.run(query)
+            raw_results = search.run(query)
+            return SearchResults(results=[SearchResult(**result) for result in raw_results])
+        
+        def summarize_results(self, search_results: SearchResults) -> str:
+            # Gather and concatenate all search results
+            snippets = [r.snippet for r in search_results.results]
+            context = ' | Next Page | '.join(snippets)
+
+            # Summarize the search results
+            summary_input = SummaryInput(snippets=snippets, context=context, question=query)
+            summary = llm.generate(
+                summary_prompt_template,
+                context=summary_input.context,
+                question=summary_input.question,
+                max_tokens=750,
+            )
+
+            return summary
         
         results = run_search(query)
         print(results)
 
         summary = summarize_results(results)
         print(summary)
-
-        def summarize_results(self, results: List[dict]) -> str:
-            # Gather and concatinate all search results
-            snippets = [result['snippet'] for result in results]
-            context = ' | Next Page | '.join(snippets)
-
-            #Summarize the search results.
-            summary = llm.generate(
-                summary_prompt_template,
-                context=context,
-                question=query,
-                max_tokens=750,
-            )
-
-            return summary
 
         return run_search
 
